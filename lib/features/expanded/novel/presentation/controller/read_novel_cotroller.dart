2cfb1a91cf34330254a3b2e4d2525783ef20c5c4
roll-t <phuoctruong727@gmail.com>
2024-10-01 15:22:08 +0700

    labor form

--- lib/features/expanded/novel/presentation/controller/read_novel_cotroller.dart
+++ lib/features/expanded/novel/presentation/controller/read_novel_cotroller.dart
@@ -1,4 +1,7 @@
+import 'dart:async'; // Import for Timer
+
 import 'package:flutter/widgets.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:get/get.dart';
 import 'package:reading_app/core/configs/default_data.dart';
 import 'package:reading_app/core/database/data/model/chapter_novel_model.dart';
@@ -7,10 +10,23 @@ class ReadNovelCotroller extends GetxController {
   ChapterNovelModel chapterNovelModel = DefaultData.defaultChapter;
   List<ChapterNovelModel> listChapter = <ChapterNovelModel>[];
   ScrollController scrollController = ScrollController();
+  var currentActiveList = 0.obs;
+
+  var isControlsVisible = true.obs;
+
+  Timer? _hideControlsTimer;
+
+  bool _hasScrolledToBottom = false;
 
   @override
   void onInit() {
     super.onInit();
+    // Show controls for 2 seconds when the page is first loaded
+    _startHideControlsTimer();
+
+    // Add a listener to the scroll controller
+    scrollController.addListener(_scrollListener);
+
     if (Get.arguments != null && Get.arguments is Map<String, dynamic>) {
       if (Get.arguments['chapterCurrent'] is ChapterNovelModel) {
         chapterNovelModel = Get.arguments['chapterCurrent'];
@@ -18,25 +34,132 @@ class ReadNovelCotroller extends GetxController {
       if (Get.arguments['listChapter'] is List<ChapterNovelModel>) {
         listChapter = Get.arguments['listChapter'];
       }
-
     }
   }
 
-  // Hàm cuộn lên
+  @override
+  void onClose() {
+    _hideControlsTimer?.cancel(); 
+    scrollController.removeListener(_scrollListener);
+    super.onClose();
+  }
+
+  void _startHideControlsTimer() {
+    _hideControlsTimer = Timer(const Duration(seconds: 5), () {
+      isControlsVisible.value = false; // Hide controls after 2 seconds
+    });
+  }
+
+  void toggleControlVisibility() {
+    isControlsVisible.value = !isControlsVisible.value; // Toggle visibility
+    // Restart timer to hide controls after 2 seconds if they are shown
+    if (isControlsVisible.value) {
+      _hideControlsTimer?.cancel(); // Cancel existing timer
+      _hideControlsTimer = Timer(const Duration(seconds: 1000), () {
+        isControlsVisible.value = false; // Hide controls after 2 seconds
+      });
+    } else {
+      // If controls are being hidden, cancel the timer
+      _hideControlsTimer?.cancel();
+    }
+  }
+
+  void clickControl() {
+    // Called when a control is clicked
+    isControlsVisible.value = true; // Ensure controls remain visible
+    _hideControlsTimer?.cancel(); // Cancel the timer if visible
+    _hideControlsTimer = Timer(const Duration(seconds: 5), () {
+      isControlsVisible.value = false; // Hide controls after 2 seconds
+    });
+  }
+
+  void changChapter({required String chapterId}) {
+    ChapterNovelModel? selectedChapter = listChapter.firstWhere(
+      (chapter) => chapter.chapterId == chapterId, // Condition to find chapter
+    );
+    if (selectedChapter != null) {
+      chapterNovelModel = selectedChapter;
+      updateChapter();
+    } else {
+      print("Chapter not found with id: $chapterId");
+    }
+  }
+
+  void updateChapter() {
+    scrollTop();
+    update(["listSelected", "mainContentChapter"]);
+  }
+
+  int currentIndexChapter({required String chapterId}) {
+    int index = listChapter.indexWhere(
+      (chapter) => chapter.chapterId == chapterId,
+    );
+    return index;
+  }
+
+  void nextChapter({required chapterId}) {
+    int currentIndex = currentIndexChapter(chapterId: chapterId);
+    if (currentIndex + 1 > listChapter.length - 1) {
+      Fluttertoast.showToast(msg: "Đây là chapter mới nhất");
+      return;
+    }
+    chapterNovelModel = listChapter[currentIndex + 1];
+    scrollTop();
+    Fluttertoast.showToast(msg: chapterNovelModel.chapterName);
+    updateChapter();
+  }
+
+  void preChapter({required chapterId}) {
+    int currentIndex = currentIndexChapter(chapterId: chapterId);
+    if (currentIndex == 0) {
+      Fluttertoast.showToast(msg: "Đây là chapter đầu tiên");
+      return;
+    }
+    chapterNovelModel = listChapter[currentIndex - 1];
+    scrollTop();
+    Fluttertoast.showToast(msg: chapterNovelModel.chapterName);
+    updateChapter();
+  }
+
+  // Scroll up
   void scrollUp() {
     scrollController.animateTo(
-      scrollController.position.pixels - 230, // Cuộn lên 100 pixels
+      scrollController.position.pixels - 230, // Scroll up 230 pixels
       duration: const Duration(milliseconds: 400),
       curve: Curves.easeOut,
     );
   }
 
-  // Hàm cuộn xuống
+  // Scroll down
   void scrollDown() {
     scrollController.animateTo(
-      scrollController.position.pixels + 230, // Cuộn xuống 100 pixels
+      scrollController.position.pixels + 230, // Scroll down 230 pixels
       duration: const Duration(milliseconds: 400),
       curve: Curves.easeOut,
     );
   }
+
+  void scrollTop() {
+    scrollController.animateTo(
+      0.0,
+      duration: const Duration(milliseconds: 400),
+      curve: Curves.easeIn,
+    );
+  }
+
+  void _scrollListener() {
+    // Check if scrolled close enough to the bottom
+    if (scrollController.position.pixels >=
+            scrollController.position.maxScrollExtent - 50 &&
+        !_hasScrolledToBottom) {
+      onScrolledToBottom();
+    }
+  }
+
+  void onScrolledToBottom() {
+    _hasScrolledToBottom = true;
+    nextChapter(chapterId: chapterNovelModel.chapterId);
+    _hasScrolledToBottom=false;
+  }
+
 }
--- lib/features/expanded/novel/presentation/page/read_novel_page.dart
+++ lib/features/expanded/novel/presentation/page/read_novel_page.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_balloon_slider/flutter_balloon_slider.dart';
 import 'package:get/get.dart';
 import 'package:reading_app/core/configs/dimens/radius_dimens.dart';
+import 'package:reading_app/core/configs/dimens/space_dimens.dart';
 import 'package:reading_app/core/configs/themes/app_colors.dart';
 import 'package:reading_app/core/ui/customs_widget_theme/texts/text_large_bold.dart';
+import 'package:reading_app/core/ui/customs_widget_theme/texts/text_medium_bold.dart';
 import 'package:reading_app/core/ui/widgets/icons/leading_icon_app_bar.dart';
+import 'package:reading_app/core/ui/widgets/modal/custom_bottom_sheet.dart';
 import 'package:reading_app/core/ui/widgets/text/text_widget.dart';
 import 'package:reading_app/features/expanded/novel/presentation/controller/read_novel_cotroller.dart';
 
@@ -13,21 +17,252 @@ class ReadNovelPage extends GetView<ReadNovelCotroller> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            const SizedBox(
-              height: 20,
+      drawer: _SideBar(),
+      body: _BuildBodyChapter(context),
+    );
+  }
+
+  // ignore: non_constant_identifier_names
+  Stack _BuildBodyChapter(BuildContext context) {
+    return Stack(
+      children: [
+        GestureDetector(
+          onTapUp: (TapUpDetails details) {
+            if (details.localPosition.dy < Get.height / 2) {
+              controller.scrollUp();
+            } else {
+              controller.scrollDown();
+            }
+          },
+          onLongPress: () {
+            controller.toggleControlVisibility(); // Toggle visibility
+          },
+          child: SingleChildScrollView(
+            controller: controller.scrollController,
+            child: GetBuilder<ReadNovelCotroller>(
+              id: "mainContentChapter",
+              builder: (_) {
+                return Column(
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.all(20),
+                      height: Get.height * .7,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Row(
+                            children: [
+                              Icon(Icons.settings),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              TextLargeBold(textChild: "Tùy chỉnh",)
+                            ],
+                          ),
+                          const SizedBox(
+                            height: SpaceDimens.space10,
+                          ),
+                          Container(
+                            width: Get.width,
+                            padding:
+                                const EdgeInsets.all(SpaceDimens.spaceStandard),
+                            decoration: BoxDecoration(
+                              color: AppColors.tertiaryDarkBg,
+                              borderRadius: BorderRadius.circular(
+                                  RadiusDimens.radiusSmall2),
+                            ),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const TextMediumBold(textChild: "Cỡ chữ"),
+                                BalloonSlider(
+                                    value: 0.16,
+                                    ropeLength: 55,
+                                    showRope: true,
+                                    onChangeStart: (val) {
+                                      val = 10;
+                                    },
+                                    onChanged: (val) {},
+                                    onChangeEnd: (val) {
+                                      val = 20;
+                                    },
+                                    color: AppColors.accentColor)
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.only(top: 20, bottom: 15),
+                      child: TextLargeBold(
+                        textChild: controller.chapterNovelModel.chapterName,
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 10),
+                      child: TextWidget(
+                        text: controller.chapterNovelModel.chapterContent,
+                      ),
+                    ),
+                  ],
+                );
+              },
             ),
-            const Padding(
-              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-              child: TextLargeBold(textChild: "Danh sách chương"),
+          ),
+        ),
+        Positioned(
+            top: 30,
+            left: 16,
+            child: Obx(() => AnimatedSlide(
+                  offset: controller.isControlsVisible.value
+                      ? const Offset(0, 0)
+                      : const Offset(-1.5, 0),
+                  curve: Curves.bounceInOut,
+                  duration: const Duration(milliseconds: 200),
+                  child: const leadingIconAppBar(),
+                ))),
+        _BuildTagControlChapter(context), // Control buttons
+      ],
+    );
+  }
+
+  // ignore: non_constant_identifier_names
+  Positioned _BuildTagControlChapter(BuildContext context) {
+    return Positioned(
+      bottom: 20,
+      left: 0,
+      right: 0,
+      child: Obx(() {
+        return AnimatedSlide(
+          curve: Curves.bounceInOut,
+          duration: const Duration(milliseconds: 200),
+          offset: controller.isControlsVisible.value
+              ? const Offset(0, 0)
+              : const Offset(0, 1.5),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: Get.width * .7,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    width: 1,
+                    color: AppColors.white.withOpacity(.4),
+                  ),
+                  borderRadius:
+                      BorderRadius.circular(RadiusDimens.radiusSmall2),
+                  color: AppColors.gray3.withOpacity(.9),
+                ),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 10, horizontal: 20),
+                      child: TextWidget(
+                        text: controller.chapterNovelModel.chapterTitle,
+                        maxLines: 1,
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        IconButton(
+                          onPressed: () {
+                            controller.clickControl();
+                            controller.preChapter(
+                                chapterId:
+                                    controller.chapterNovelModel.chapterId);
+                          },
+                          icon: const Icon(Icons.arrow_back_ios_new_rounded),
+                        ),
+                        Builder(
+                          builder: (BuildContext context) {
+                            return IconButton(
+                              onPressed: () {
+                                controller.clickControl();
+                                Scaffold.of(context).openDrawer();
+                              },
+                              icon: const Icon(Icons.list),
+                            );
+                          },
+                        ),
+                        IconButton(
+                          onPressed: () {
+                            controller.clickControl();
+                          }, // Placeholder for comments
+                          icon: const Icon(Icons.comment),
+                        ),
+                        IconButton(
+                          onPressed: () {
+                            controller.clickControl();
+                            _BuildButtomSheet(context).show(context);
+                          }, // Placeholder for comments
+                          icon: const Icon(Icons.settings),
+                        ),
+                        IconButton(
+                          onPressed: () {
+                            controller.clickControl();
+                            controller.nextChapter(
+                                chapterId:
+                                    controller.chapterNovelModel.chapterId);
+                          },
+                          icon: const Icon(Icons.arrow_forward_ios_rounded),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        );
+      }),
+    );
+  }
+
+  // ignore: non_constant_identifier_names
+  CustomBottomSheetWidget _BuildButtomSheet(BuildContext context) {
+    return CustomBottomSheetWidget(
+      paddingContent: const EdgeInsets.all(SpaceDimens.space20),
+      heightSheet: Get.height * .7,
+      context,
+      viewItems: const [
+        Row(
+          children: [
+            Icon(Icons.settings),
+            SizedBox(
+              width: 10,
             ),
-            for (var i = 0; i < controller.listChapter.length; i++)
-              InkWell(
-                onTap: () {},
-                child: Container(
+            TextLargeBold(textChild: "Tùy chỉnh")
+          ],
+        )
+      ],
+    );
+  }
+
+  // Build the sidebar
+  // ignore: non_constant_identifier_names
+  Drawer _SideBar() {
+    return Drawer(
+      child: GetBuilder<ReadNovelCotroller>(
+        id: "listSelected",
+        builder: (_) {
+          return ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              const SizedBox(height: 20),
+              const Padding(
+                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                child: TextLargeBold(textChild: "Danh sách chương"),
+              ),
+              for (var i = 0; i < controller.listChapter.length; i++)
+                InkWell(
+                  onTap: () {
+                    controller.changChapter(
+                        chapterId: controller.listChapter[i].chapterId);
+                  },
+                  child: Container(
                     decoration: BoxDecoration(
                       color: controller.chapterNovelModel.chapterId ==
                               controller.listChapter[i].chapterId
@@ -37,95 +272,15 @@ class ReadNovelPage extends GetView<ReadNovelCotroller> {
                     padding: const EdgeInsets.symmetric(
                         vertical: 10, horizontal: 20),
                     child: TextWidget(
-                        maxLines: 2,
-                        text:
-                            "${controller.listChapter[i].chapterName}: ${controller.listChapter[i].chapterTitle}")),
-              ),
-          ],
-        ),
-      ),
-      body: Stack(
-        children: [
-          GestureDetector(
-            onTapUp: (TapUpDetails details) {
-              if (details.localPosition.dy < Get.height / 2) {
-                controller.scrollUp();
-              } else {
-                controller.scrollDown();
-              }
-            },
-            child: SingleChildScrollView(
-              controller: controller.scrollController,
-              child: Column(
-                children: [
-                  Container(
-                    padding: const EdgeInsets.only(top: 20, bottom: 15),
-                    child: TextLargeBold(
-                      textChild: controller.chapterNovelModel.chapterName,
+                      maxLines: 2,
+                      text:
+                          "${controller.listChapter[i].chapterName}: ${controller.listChapter[i].chapterTitle}",
                     ),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.symmetric(horizontal: 10),
-                    child: TextWidget(
-                      text: controller.chapterNovelModel.chapterContent,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          const Positioned(top: 30, left: 16, child: leadingIconAppBar()),
-          Positioned(
-            bottom: 20,
-            left: 0,
-            right: 0,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  width: Get.width * .6,
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      width: 1,
-                      color: AppColors.white.withOpacity(.7),
-                    ),
-                    borderRadius:
-                        BorderRadius.circular(RadiusDimens.radiusSmall2),
-                    color: AppColors.gray3.withOpacity(.5),
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      IconButton(
-                        onPressed: () {},
-                        icon: const Icon(Icons.arrow_back_ios_new_rounded),
-                      ),
-                      // Sử dụng Builder để có được ngữ cảnh chính xác
-                      Builder(
-                        builder: (BuildContext context) {
-                          return IconButton(
-                            onPressed: () {
-                              Scaffold.of(context).openDrawer();
-                            },
-                            icon: const Icon(Icons.list),
-                          );
-                        },
-                      ),
-                      IconButton(
-                        onPressed: () {},
-                        icon: const Icon(Icons.comment),
-                      ),
-                      IconButton(
-                        onPressed: () {},
-                        icon: const Icon(Icons.arrow_forward_ios_rounded),
-                      ),
-                    ],
                   ),
                 ),
-              ],
-            ),
-          ),
-        ],
+            ],
+          );
+        },
       ),
     );
   }
